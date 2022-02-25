96469a5491648214f3f716c74f4ed321f5586382
Juan Diego Diaz Ochoa <jd.diazo@uniandes.edu.co>
2022-02-24 23:53:26 -0500

    Primera entrega – Reto 1

--- App/view.py
+++ App/view.py
@@ -33,11 +33,30 @@ Presenta el menu de opciones y por cada seleccion
 se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
+def newController():
+    """
+    Se crea una instancia del controlador
+    """
+    control = controller.newController()
+    return control
 
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2- Listar los álbunes en un periodo de tiempo")
+    print("3- Encontrar los artistas más populares")
+    print("4- Encontrar las canciones más populares")
+    print("5- Encontrar la canción más popular de un artista")
+    print("6- Encontar la discografía de un artista")
+    print("7- Clasificar las canciones con mayor distribución")
+    print("0- Salir")
+
+def loadData():
+    """
+    Solicita al controlador que cargue los datos en el modelo
+    """
+    artists, albums, tracks = controller.loadData(control)
+    return artists, albums, tracks 
 
 catalog = None
 
@@ -49,10 +68,22 @@ while True:
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
         print("Cargando información de los archivos ....")
-
+        artist,albums,tracks=loadData()
     elif int(inputs[0]) == 2:
-        pass
-
+        p_tiempo=input('Ingrese el periodo de tiempo del cual desea listar los albunes: ')
+        print('Linstando albunes...')
+    elif int(inputs[0])==3:
+        print('Encontrando los artistas mas populares...')
+    elif int(inputs[0])==4:
+        print('Encontrando las canciones mas populares...')
+    elif int(inputs[0])==5:
+        artista=input('Ingrese el nombre del artitas del que desea saber su cancion mas popular: ')
+        print('Encontrando la cancion mas popular del artista...')
+    elif int(inputs[0])==6:
+        artista=input('Ingrese el nombre del artitas del que desea saber su discografia: ')
+        print('Encontrando la discografia del artista...')
+    elif int(inputs[0])==7:
+        print('Clasificando las canciones con mayor distribución...')
     else:
         sys.exit(0)
 sys.exit(0)
